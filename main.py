b2124c8a4efe16efe5e46698c4c54fbe573fd216
PhilippeJoh <78028710+PhilippeJoh@users.noreply.github.com>
2021-03-17 09:14:18 -0400

    completer ex 1
    l'exercice 1 a été complété dans cette version, les cercle ne bouge plus et ne clignote plus. commentaire ajouté et retrait de toute les erreur
    
    **version stable**

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import arcade
 import random
+from dataclasses import dataclass  # ligne 1 à 3 import librairie
+# ligne 5 à 25 déclaration des variable
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+COLORS = [arcade.color.PINK, arcade.color.PURPLE,
+          arcade.color.BLUE, arcade.color.GREEN,
+          arcade.color.RED]
+
+
+@dataclass
+class Cercle:
+    centre_x: int
+    centre_y: int
+    rayon: int
+    color: (int, int, int)
+
+    def draw(self):
+        arcade.draw_circle_filled(self.centre_x,
+                                  self.centre_y,
+                                  self.rayon,
+                                  self.color)
 
 
 class MyGame(arcade.Window):
-    def __init__(self, width, height, title):
-        # Call the parent class's init function
-        super().__init__(width, height, title)
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice arcade #1")
+        self.liste_cercles = []
 
+    def setup(self):
 
+        for _ in range(20):
+            rayon = random.randint(10, 50)
+            centre_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
+            centre_y = random.randint(0 + rayon, SCREEN_HEIGHT - rayon)
+            couleur = random.choice(COLORS)
+            self.liste_cercles.append(Cercle(centre_x, centre_y, rayon, couleur))
+
+    def on_draw(self):
+        arcade.start_render()
+        for cercle in self.liste_cercles:
+            cercle.draw()
 
 
 def main():
-
-    window = MyGame(640, 480, "Drawing Example")
-# def on_draw(self):
-    liste_couleur = [arcade.color.BLUE, arcade.color.RED, arcade.color.GREEN]
-    arcade.start_render()
-    i = 0
-    while i < 20:
-        couleur = random.choice(liste_couleur)
-        rayon = random.randint(1, 20)
-        cordone_x = random.randint(0+rayon, 640-rayon)
-        cordone_y = random.randint(0+rayon, 480-rayon)
-        arcade.draw_circle_filled(cordone_x, cordone_y, rayon, couleur)
-        i += 1
+    my_game = MyGame()
+    my_game.setup()
 
     arcade.run()
 
